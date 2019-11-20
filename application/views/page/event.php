c30c0017fc2f6472e346a2186b9072013f3c80b5
elydesyana <elydesyana@gmail.com>
2019-11-20 16:59:55 +0700

    merapikan tampilan

--- application/views/page/event.php
+++ application/views/page/event.php
@@ -10,8 +10,26 @@
                     <iframe width="500" height="315" src="https://www.youtube.com/embed/<?=$r->thumbnail?>" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
                 <div class="col-md-6">
-                    <h3 class="m-b-10"><?=$r->judul?></h3>
-                    <p class="m-text1"><?=$this->func->potong(strip_tags($r->konten),300,"...")?></p>
+					<h3 class="m-t-20 m-b-20"><?=$r->judul?></h3>
+					<div class="m-b-20">
+						<ul class="meta clr">
+							<li class="meta-author">
+								<i class="lnr lnr-user p-r-5"></i>
+								<a href="#">Soloradio</a>
+							</li>
+							<li class="meta-date">
+								<i class="lnr lnr-clock p-r-5"></i>
+								<a href="#">Kamis, 21 Nov 2019</a>
+							</li>
+							<li class="meta-tag">
+								<i class="lnr lnr-tag p-r-5"></i>
+								<a href="#">Event</a>
+							</li>
+						</ul>
+					</div>
+					<p class="m-t-20 m-text1"><?=$this->func->potong(strip_tags($r->konten),300,"...")?> 
+						<a href="#" class="m-t-20" style="font-size: 16px;">Selengkapnya</a>
+					</p>
                 </div>
             </div>
 		<?php
